78d25a93bdece6e70f554c8a59eb03ed1863d3e5
Roxana149 <r.soto@uniandes.edu.co>
2026-02-05 19:52:43 -0500

    cambios en main

--- App/logic.py
+++ App/logic.py
@@ -133,17 +133,14 @@ def load_books_tags(catalog, filename):
     :rtype: int
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    catalog=control["model"]
-    booksfile=os.path.join(cf.data_dir, filename)
-    catalog=model.addBookTags(catalog, booksfile)
-    return model.bookTagSize(catalog)
-
     tf= os.path.join(data_dir, filename)
     input_file=csv.DictReader(open(tf,encoding="utf-8"))
     catalog["model"]=create_book_tag_list(catalog["model"])
     for booktag in input_file:
         add_book_tag(catalog, booktag)
     return book_tag_size(catalog)
+
+    
 
 def first_book(catalog):
     """
